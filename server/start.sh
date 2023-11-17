71a0a9c37dfdb780ea944029f708129872f18e32
Miroslav <miroslav@chalko.sk>
2023-11-17 09:18:15 +0000

    created startup & terminate scripts

--- server/start.sh
+++ server/start.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+mkdir -p log
 sudo systemctl start mongod
+nohup python server.py > log/coap.log 2>&1 &
+echo $! > log/coap_pid
+cd web
+nohup python web.py > ../log/web.log 2>&1 &
+echo $! > ../log/web_pid
+
--- server/stop.sh
+++ server/stop.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 sudo systemctl stop mongod
+kill -9 `cat log/web_pid`
+rm log/web_pid
+kill -9 `cat log/coap_pid`
+rm log/coap_pid
